03369753360573b01e771348dc0cf89a741dd465
Ming <ming@Mings-MacBook-Pro.local>
2014-10-24 16:22:58 +0800

    Allow get category parent and child

--- src/Webshaper/Core/Models/Category.php
+++ src/Webshaper/Core/Models/Category.php
@@ -8,5 +8,13 @@ class Category extends BaseModel{
     protected $table = "category1" ;
     protected $fillable=[];
 
+    public function parent()
+    {
+        return $this->belongsTo('Webshaper\Core\Models\Category','intPKParent','intPKCategory');
+    }
 
+    public function childs()
+    {
+        return $this->hasMany('Webshaper\Core\Models\Category','intPKCategory','intPKParent');
+    }
 }
--- src/Webshaper/Core/Repository/CategoryRepository.php
+++ src/Webshaper/Core/Repository/CategoryRepository.php
@@ -1,11 +1,25 @@
 <?php namespace Webshaper\Core\Repository;
 
 use Webshaper\Core\Models\Category;
+use Webshaper\Core\Models\ProductItem;
 class CategoryRepository extends BaseRepository{
 
     function __construct(Category $model){
         parent::__construct($model);
     }
 
+    public function getAllMain()
+    {
+        $distinctCatId = ProductItem::distinct('intPKCategory')->get(array('intPKCategory'))->toArray();
+        $categories = Category::where('intActive',1)->whereIn('intPKCategory',$distinctCatId)->get();
 
+        return $categories;
+    }
+
+    public function getLastModified()
+    {
+        $category = Category::limit(1)->orderBy('dtModified','desc')->get();
+
+        return $category[0];
+    }
 } 
